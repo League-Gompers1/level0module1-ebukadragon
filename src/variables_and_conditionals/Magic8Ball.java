d4bf2952b91f5555b6a69115e7596eaaf9ff38b3
ebukadragon <chukwuebuka.alozie@gompersstudent.org>
2017-05-01 15:00:40 -0700

    Add files via upload

--- src/variables_and_conditionals/Magic8Ball.java
+++ src/variables_and_conditionals/Magic8Ball.java
@@ -2,18 +2,24 @@ package variables_and_conditionals;
 
 import java.util.Random;
 
+import javax.swing.JOptionPane;
+
 public class Magic8Ball {
 
     // 1. Make a main method that includes all the steps below….
-
+       public static void main(String[] args) {
+		
+	
     // 2. Make a variable that will hold a random number and put a random number into this variable using "new Random().nextInt(4)"
-
+   Random R1=new Random();
+    	   int ran=R1.nextInt(4);
+    	  
     // 3. Print out this variable
-
+      System.out.println(ran); 
     // 4. Get the user to enter a question for the 8 ball
-
+      String Question =JOptionPane.showInputDialog("Question");
     // 5. If the random number is 0
-
+      if(ran==0){ JOptionPane.showMessageDialog(null, "yes");
     // -- tell the user "Yes"
 
     // 6. If the random number is 1
@@ -27,5 +33,5 @@ public class Magic8Ball {
     // 8. If the random number is 3
 
     // -- write your own answer
-
-}
+       }
+}}
--- src/variables_and_conditionals/XGeeks.java
+++ src/variables_and_conditionals/XGeeks.java
@@ -11,10 +11,29 @@ public class XGeeks {
 public static void main(String[] args) {
 
         // 1. Save the superpower for each person in a variable.
-
+			String Daniel ="hacking";
+			String Ebuka = "endurance";
+		    String christopher ="reading";
+		    String Jesus ="speed";
         // 2. Ask the user to enter a name. Store their answer in a variable.
-
+               String name =JOptionPane.showInputDialog("enter a name");
         // 3. Show the superpower in a pop-up, depending on the name entered. 
-
-    }
+              if(name.equalsIgnoreCase("Daniel")){
+            	  JOptionPane.showMessageDialog(null, "Dainels super power is "+Daniel);
+            	  
+            	  
+              }else if(name.equalsIgnoreCase("Ebuka")){
+            	  JOptionPane.showMessageDialog(null, "Ebukas super power is "+Ebuka);
+}
+              else if(name.equalsIgnoreCase("christopher")){
+            	  JOptionPane.showMessageDialog(null, "christopher super power is "+christopher);
+}
+              else if(name.equalsIgnoreCase("jesus")){
+            	  JOptionPane.showMessageDialog(null, "jesus super power is "+Jesus);
+            	  
+            	  
+}           	              	  
+              else {
+            	  JOptionPane.showMessageDialog(null, "what is 2+2");
+              }}            
 }
